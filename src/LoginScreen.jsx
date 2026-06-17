6401f7440475d04d29d24d32f1bd0d2d4f59df00
leilianeva-cloud <leiliane.v.a@gmail.com>
2026-06-17 17:24:23 -0300

    Reduce opacity of background element on LoginScreen

--- src/LoginScreen.jsx
+++ src/LoginScreen.jsx
@@ -45,7 +45,7 @@ export default function LoginScreen() {
   left: -160px;
   bottom: -220px;
   width: 900px;       /* ajuste o tamanho conforme a imagem */
-  opacity: .08;
+  opacity: .045;
   z-index: 0;
   pointer-events: none;
   user-select: none;
