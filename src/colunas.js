e2385aa99b8fa5d5f336a536c600346b550432dd
leilianeva-cloud <leiliane.v.a@gmail.com>
2026-08-26 13:23:19 -0300

    Reorder SQUAD entries in colunas.js

--- src/colunas.js
+++ src/colunas.js
@@ -56,7 +56,7 @@ export const CAMPOS_PORTFOLIO = {
   DESPRI:      ['despriorizacao'],
   FASE:        ['fase do projeto'],
   TARGET:      ['data de termino plan', 'data de termino planejada'],
-  SQUAD:       ['squad atualizada', 'squad azure'],
+  SQUAD:       ['squad azure', 'squad atualizada'],
   REPLAN:      ['replanejamento', 'status lecom'],
 };
 
@@ -82,7 +82,7 @@ export const CAMPOS_MELHORIAS = {
   TARGET: ['p prevista', 'p. prevista', 'data prevista'],
   STATUS: ['status'],
   LIDER:  ['gerente ti'],
-  SQUAD:  ['squad atualizada', 'nome squad azure'],
+  SQUAD:  ['nome squad azure', 'squad atualizada'],
   REPLAN: ['replanejamento'],
 };
 
